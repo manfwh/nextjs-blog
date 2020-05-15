04d11b01bc0ee19a5c402b77dfb0fd7913085d63
manfwh <841533182@qq.com>
2020-05-15 20:22:57 +0800

    uppdate

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Layout from '../../components/layout'
 import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
 export default function Post({ postData }) {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
   return (
     <Layout>
       <Head>
@@ -24,12 +29,13 @@ export async function getStaticPaths() {
   const paths = getAllPostIds()
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
   return {
+    unstable_revalidate: 1,
     props: {
       postData
     }
